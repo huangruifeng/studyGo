44e29f40acec58f8ec117a5a1dcf5e946204732f
黄瑞峰 <huangruifeng@cvte.com>
2020-12-10 21:26:17 +0800

    [modify][what]增加package Test

---
+++ 18/client/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/huangruifeng/studyGo/18/packageTest"
+
+func main() {
+	packageTest.HelloWorld()
+}
---
+++ 18/packageTest/packageTest.go
@@ -0,0 +1,23 @@
+package packageTest
+
+import "fmt"
+
+func init() {
+	fmt.Println("packageTest init3!")
+}
+
+func init() {
+	fmt.Println("packageTest init1!")
+}
+
+func init() {
+	fmt.Println("packageTest init2!")
+}
+
+func helloWorld() {
+	fmt.Println("hello world!")
+}
+
+func HelloWorld() {
+	fmt.Println("Hello world")
+}
